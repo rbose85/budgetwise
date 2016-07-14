fc9605f5c83258a7a34ba8abf79d87a834c19726
Rajiv Bose <rbose85@users.noreply.github.com>
2016-07-14 17:42:41 +0100

    working example with github

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
+import Bell from 'bell'
 import Hapi from 'hapi'
 
 // Create a server with a host and port
-const server = new Hapi.Server()
-server.connection({
-  host: 'localhost',
-  port: 8000
+const server = new Hapi.Server({
+  debug: { request: ['error'] }
 })
 
-// Add the route
-server.route({
-  method: 'GET',
-  path: '/hello',
-  handler: (request, reply) => reply('hello world')
-})
+server.connection({ host: 'localhost', port: 8000 })
 
-// Start the server
-server.start((err) => {
+server.register(Bell, (err) => {
   if (err) {
-    throw err
+    console.log(err) // eslint-disable-line no-console
+    process.exit(1)
   }
 
-  console.log('Server running at:', server.info.uri) // eslint-disable-line no-console
+  server.auth.strategy('github-oauth', 'bell', {
+    provider: 'github',
+    password: process.env.COOKIE_ENCRYPTION_PASSWORD,
+    clientId: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    isSecure: false
+  })
+
+  server.route({
+    method: ['GET', 'POST'],
+    path: '/login',
+    config: {
+      auth: 'github-oauth',
+      handler: (req, reply) => {
+        if (!req.auth.isAuthenticated) {
+          return reply(`Authentication failed: ${req.auth.error.message}`)
+        }
+        return reply.redirect('/home')
+      }
+    }
+  })
+
+  // Start the server
+  server.start((e) => {
+    if (e) {
+      throw e
+    }
+
+    console.log('Server running at:', server.info.uri) // eslint-disable-line no-console
+  })
 })
