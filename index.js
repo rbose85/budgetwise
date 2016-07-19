dc74e8dfe3fd24900a05e3747c0ec5dad6fdc9f2
Rajiv Bose <rbose85@users.noreply.github.com>
2016-07-19 16:04:16 +0200

    not working splitwise

--- index.js
+++ index.js
@@ -10,27 +10,52 @@ server.connection({ host: 'localhost', port: 8000 })
 
 server.register(Bell, (err) => {
   if (err) {
-    console.log(err) // eslint-disable-line no-console
+    console.log(`err: ${err}`) // eslint-disable-line no-console
     process.exit(1)
   }
 
-  server.auth.strategy('github-oauth', 'bell', {
-    provider: 'github',
-    password: process.env.COOKIE_ENCRYPTION_PASSWORD,
-    clientId: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    isSecure: false
+  const URL = 'https://secure.splitwise.com'
+  const API = `${URL}/api/v3.0`
+  server.auth.strategy('something', 'bell', {
+    provider: {
+      protocol: 'oauth',
+      signatureMethod: 'HMAC-SHA1',
+      temporary: `${API}/get_request_token`,
+      auth: `${URL}/authorize`,
+      token: `${API}/get_access_token`,
+      version: '1.0',
+      headers: {},
+      profile: (credentials, params, _get, cb) => {
+        console.log(`credentials: ${credentials}`) // eslint-disable-line no-console
+        console.log(`params: ${params}`) // eslint-disable-line no-console
+
+        _get(`${API}/get_current_user`, { format: 'json' }, (profile) => {
+          console.log(`profile: ${profile}`) // eslint-disable-line no-console
+
+          return cb()
+        })
+      }
+    },
+    password: process.env.SPLITWISE_ENCRYPTION_PASSWORD,
+    clientId: process.env.BUDGETWISE_CONSUMER_KEY,
+    clientSecret: process.env.BUDGETWISE_CONSUMER_SECRET,
+    forceHttps: true,
+    isSecure: true
   })
 
   server.route({
     method: ['GET', 'POST'],
     path: '/login',
     config: {
-      auth: 'github-oauth',
+      auth: 'something',
       handler: (req, reply) => {
         if (!req.auth.isAuthenticated) {
+          const error = `!isAuthenticated ... ${JSON.stringify(req.auth)}`
+          console.log(error) // eslint-disable-line no-console
           return reply(`Authentication failed: ${req.auth.error.message}`)
         }
+        const msg = `isAuthenticated ... ${JSON.stringify(req.auth)}`
+        console.log(msg) // eslint-disable-line no-console
         return reply.redirect('/home')
       }
     }
